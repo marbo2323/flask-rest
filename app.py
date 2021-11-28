dc11f800c0658b88d115b9401cfa49f531711d63
Arbo Lindma <arbo.lindma@gmail.com>
2021-11-28 23:50:07 +0200

    added blueprints

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 import  models
+from resources.courses import courses_api
+from resources.reviews import reviews_api
 
 DEBUG = True
 HOST = '0.0.0.0'
 PORT = 8000
 
+API_PREFIX = "/api/v1"
+
 app = Flask(__name__)
+app.register_blueprint(courses_api)
+app.register_blueprint(reviews_api, url_prefix=API_PREFIX)
 
 @app.route('/')
 def hello_world():
--- resources/reviews.py
+++ resources/reviews.py
@@ -1,5 +1,5 @@
-from flask import jsonify
-from flask_restful import Resource
+from flask import jsonify, Blueprint
+from flask_restful import Resource, Api
 import models
 
 
@@ -17,3 +17,16 @@ class Review(Resource):
 
     def delete(self: id):
         return jsonify({"course": 1, "rating": 5})
+
+reviews_api = Blueprint('reviews', __name__)
+api = Api(reviews_api)
+api.add_resource(
+    RevieweList,
+    '/reviews',
+    endpoint='reviews'
+)
+api.add_resource(
+    Review,
+    '/reviews/<int:id>',
+    endpoint='review'
+)
